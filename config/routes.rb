d7cf94d76a4f3a773694aa546852e8acb7cfeb07
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-04-05 14:31:43 +0200

    all the routes are done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :quotes
+  resources :quotes do
+    resources :lines, only: [] do
+      resources :services, only: [:new, :create, :update, :edit]
+    end
+  end
+  resources :products, only: [:new, :create] do
+    resources :supplies, only: [:index, :new, :create]
+  end
   resources :customers, only: [:new, :create]
+  resources :categories, only: [:new, :create]
 end
