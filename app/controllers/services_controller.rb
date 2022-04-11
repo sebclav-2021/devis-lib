acd8ae6019f0b454f004290c46a4883f93c7358c
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-04-11 12:37:59 +0200

    services creation

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,5 +1,5 @@
 class ServicesController < ApplicationController
-  before_action :set_service, only: [:edit, :update]
+  before_action :set_service, only: [:edit, :update, :show]
 
   def new
     @category = Category.find(params[:category_id])
@@ -12,15 +12,30 @@ class ServicesController < ApplicationController
     @service.user = current_user
     @service.category = @category
     if @service.save
-      redirect_to categories_path
+      redirect_to @service
     else
-      redirect_to new_quote_line_service_path
+      render 'categories/index'
     end
   end
 
   def update
     @service.update(service_params)
     redirect_to quote_line_services_path
+  end
+
+  def show
+    @product = Product.new
+  end
+
+  def create_supply
+    @product = Product.find(params[:product_id].to_i)
+    @service = Service.find(params[:service_id].to_i)
+    @supply = Supply.new(product: @product, service: @service, quantity: params[:quantity].to_i)
+    if @supply.save
+      redirect_to @service
+    else
+      render 'show'
+    end
   end
 
   def edit
