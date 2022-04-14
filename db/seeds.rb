60f9289421adcf09edb24fc7633ee3aeca17cd01
JeanGuillaume Vicente <jg.vicente@wanadoo.fr>
2022-04-14 12:35:52 +0200

    des corrections

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,9 +36,9 @@ end
 
 Category.create!(name: "Pose d'une douche à l'italienne")
 Category.create!(name: "Remplacement d'un lavabo")
-Category.create!(name: "Pose d'un meuble salle de bain")
 Category.create!(name: "Pose d'un chauffe-eau")
 Category.create!(name: "Révision canalisations sanitaires")
+Category.create!(name: "Recherche de fuite")
 
 Product.create!(name: "Baignoire rectangulaire L.170x l.75 cm blanc", unite: "Pièce", price: "358.50", workforce: false)
 Product.create!(name: "Baignoire bain douche rectangulaire L.140x l.90 cm blanc", unite: "Pièce", price: "698.10", workforce: false)
@@ -53,6 +53,7 @@ Product.create!(name: "Joint WC pour sortie de cuvette l.11 x H.0.9 x P.7 cm", u
 Product.create!(name: "Joint WC pour soupape SAS l.9.5 x H.15 x P.1 cm", unite: "Pièce", price: "3.40", workforce: false)
 Product.create!(name: "Lavabo colonne en céramique, blanc", unite: "Pièce", price: "39.40", workforce: false)
 Product.create!(name: "Lavabo Rétro CELESTE PASTELLO 60cm", unite: "Pièce", price: "157.10", workforce: false)
+Product.create!(name: "Main d'oeuvre", unite: "Forfait", price: "150.00", workforce: false)
 Product.create!(name: "Mitigeur thermostatique de baignoire chromé", unite: "Pièce", price: "125.10", workforce: false)
 Product.create!(name: "Mitigeur thermostatique de baignoire noir mat", unite: "Pièce", price: "150.10", workforce: false)
 Product.create!(name: "Mitigeur mécanique de baignoire chromé", unite: "Pièce", price: "99.00", workforce: false)
@@ -62,6 +63,8 @@ Product.create!(name: "Pipe de WC Diam.93", unite: "Pièce", price: "15.10", wor
 Product.create!(name: "Plan vasque double Max Céramique blanc l.121 x P.46 cm", unite: "Pièce", price: "87.70", workforce: false)
 Product.create!(name: "Plan vasque simple Essential Céramique blanc l.61 x P.46 cm", unite: "Pièce", price: "15.10", workforce: false)
 Product.create!(name: "Plan vasque simple Perla Résine de synthèse blanc l.81.8 x P.48 cm", unite: "Pièce", price: "77.80", workforce: false)
+Product.create!(name: "Receveur de douche, résine, blanc L.120 x l.80 cm", unite: "Pièce", price: "124.60", workforce: false)
+Product.create!(name: "Receveur de douche, résine, noir L.140 x l.90 cm", unite: "Pièce", price: "174.60", workforce: false)
 Product.create!(name: "Robinet de baignoire à 2 poignées et déviateur de douche Chrome", unite: "Pièce", price: "197.10", workforce: false)
 Product.create!(name: "Robinet de baignoire à 2 poignées et déviateur de douche Chrome", unite: "Pièce", price: "154.60", workforce: false)
 Product.create!(name: "Vasque à poser céramique rectangulaire", unite: "Pièce", price: "89.50", workforce: false)
@@ -70,4 +73,3 @@ Product.create!(name: "Siphon extra-plat, sortie verticale, H.11 cm x l.14 cm", 
 Product.create!(name: "Siphon de cour en pvc", unite: "Pièce", price: "25.50", workforce: false)
 Product.create!(name: "Siphon sol ABS gris", unite: "Pièce", price: "32.10", workforce: false)
 Product.create!(name: "Siphonnette grille+ cadre inox 10x10", unite: "Pièce", price: "22.20", workforce: false)
-Product.create!(name: "Main d'oeuvre", unite: "Forfait", price: "50.00", workforce: false)
